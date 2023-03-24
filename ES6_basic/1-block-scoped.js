6e42565ae9bf03c94d7d00fad5c91c880c9b5d93
natyarteagac <natalia.arteagac94@gmail.com>
2023-03-23 21:45:15 -0500

    Fixed Linter

--- ES6_basic/1-block-scoped.js
+++ ES6_basic/1-block-scoped.js
@@ -1,11 +1,11 @@
 export default function taskBlock(trueOrFalse) {
     if (trueOrFalse) {
-      var task = true;
-      var task2 = false;
+        var task = true;
+        var task2 = false;
     }
 
-    var task = false;
-    var task2 = true;
+    task = false;
+    task2 = true;
 
     return [task, task2];
 }
