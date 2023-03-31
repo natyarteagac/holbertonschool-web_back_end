683eee5ced9aead1627bc81b1e540da41ee873ba
natyarteagac <natalia.arteagac94@gmail.com>
2023-03-30 21:23:19 -0500

    Fixing checker

--- ES6_data_manipulation/1-get_list_student_ids.js
+++ ES6_data_manipulation/1-get_list_student_ids.js
@@ -2,8 +2,6 @@ export default function getListStudentIds(listStudents) {
   let listResult = [];
   if (Array.isArray(listStudents)) {
     listResult = listStudents.map((student) => (student.id));
-  } else {
-    listResult = [];
   }
   return listResult;
 }
