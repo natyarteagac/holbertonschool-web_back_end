23bd85af9d5183a19f956cef40fbad50704b8092
natyarteagac <natalia.arteagac94@gmail.com>
2023-04-04 11:58:24 -0500

    fixed test errors

--- ES6_data_manipulation/5-typed_arrays.js
+++ ES6_data_manipulation/5-typed_arrays.js
@@ -1,8 +1,7 @@
 export default function createInt8TypedArray(length, position, value) {
-  if (position < 0 || position >= length) {
-    return ('Position outside range');
+  if (position >= length) {
+    throw new Error('Position outside range');
   }
-
   const buffer = new ArrayBuffer(length);
   const int8Array = new Int8Array(buffer);
   int8Array[position] = value;
