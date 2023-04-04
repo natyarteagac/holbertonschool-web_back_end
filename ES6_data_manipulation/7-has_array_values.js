76d36fe70436a7a445c013cea01bb675aa2859a2
natyarteagac <natalia.arteagac94@gmail.com>
2023-04-04 17:26:45 -0500

    Fixed eslint

--- ES6_data_manipulation/7-has_array_values.js
+++ ES6_data_manipulation/7-has_array_values.js
@@ -1,6 +1,6 @@
 export default function hasValuesFromArray(set, array) {
-  for (let i = 0; i < array.length; i++) {
-    if (!set.has(array[i])) {
+  for (const value of array) {
+    if (!set.has(value)) {
       return false;
     }
   }
